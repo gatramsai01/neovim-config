054a9fb18736eed7e5ed7391b39fe47c96c5e0f2
eshwar-gatram <eshwar.gatram@smartfalcon.io>
2024-08-13 16:45:15 +0530

    changed gitdir watch interval

--- lua/usr/gitsigns.lua
+++ lua/usr/gitsigns.lua
@@ -21,7 +21,7 @@ gitsigns.setup({
 	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
 	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 	watch_gitdir = {
-		interval = 1000,
+		interval = 500,
 		follow_files = true,
 	},
 	attach_to_untracked = true,
