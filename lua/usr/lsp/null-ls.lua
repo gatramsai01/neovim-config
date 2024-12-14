7e4c04c3bafe259f97793376e58d81ab1c5cc82e
eshwar-gatram <eshwar.gatram@smartfalcon.io>
2024-12-14 19:19:07 +0530

    added eslint and prettier config in null-ls

--- lua/usr/lsp/null-ls.lua
+++ lua/usr/lsp/null-ls.lua
@@ -8,46 +8,39 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 -- local diagnostics = null_ls.builtins.diagnostics
 -- local eslint_d=
+
+local root_has_file = function(files)
+	return function(utils)
+		return utils.root_has_file(files)
+	end
+end
+
+local eslint_root_files = { ".eslintrc", ".eslintrc.js", ".eslintrc.json" }
+local prettier_root_files = { ".prettierrc", ".prettierrc.js", ".prettierrc.json" }
+
 null_ls.setup({
 	debug = false,
 	sources = {
 		require("none-ls.formatting.eslint_d").with({
 			filetypes = { "javascript", "typescript", "vue", "html", "css" },
-			condition = function()
-				return require("null-ls.utils").root_pattern(
-					"eslint.config.js",
-					-- https://eslint.org/docs/user-guide/configuring/configuration-files#configuration-file-formats
-					".eslintrc",
-					".eslintrc.js",
-					".eslintrc.cjs",
-					".eslintrc.yaml",
-					".eslintrc.yml",
-					".eslintrc.json",
-					"package.json"
-				)(vim.api.nvim_buf_get_name(0)) ~= nil
+			condition = function(utils)
+				local has_eslint = root_has_file(eslint_root_files)(utils)
+				local has_prettier = root_has_file(prettier_root_files)(utils)
+				return has_eslint and not has_prettier
 			end,
 		}),
 		formatting.prettierd.with({
 			-- use prettier only with prettierrc present
-			condition = function()
-				return require("null-ls.utils").root_pattern(
-					".prettierrc",
-					".prettierrc.json",
-					".prettierrc.yml",
-					".prettierrc.yaml",
-					".prettierrc.json5",
-					".prettierrc.js",
-					".prettierrc.cjs",
-					".prettierrc.toml",
-					"prettier.config.js",
-					"prettier.config.cjs"
-				)(vim.api.nvim_buf_get_name(0)) ~= nil
+			condition = function(utils)
+				local has_eslint = root_has_file(eslint_root_files)(utils)
+				local has_prettier = root_has_file(prettier_root_files)(utils)
+				return not has_eslint and has_prettier
 			end,
 		}),
 		-- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
 		require("none-ls.formatting.rustfmt"),
-		-- diagnostics.flake8
+		require("none-ls.diagnostics.ruff"),
 	},
 })
