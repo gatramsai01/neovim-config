ee216c621af8d39d7292ca0a417cea6aa17e0e5c
eshwar-gatram <eshwar.gatram@smartfalcon.io>
2025-06-15 11:19:36 +0530

    feat(lsp): add Ruff LSP support and update Pyright settings
    * Add Ruff to the list of LSP servers
    * Disable hover capability for Ruff LSP
    * Update Pyright settings to use virtual environment Python path and enable basic type checking
    * Add initial settings for Ruff LSP

--- lua/usr/lsp/mason.lua
+++ lua/usr/lsp/mason.lua
@@ -10,7 +10,8 @@ local servers = {
 	"jsonls",
 	"yamlls",
 	"tailwindcss",
-    "helm_ls"
+	"helm_ls",
+	"ruff",
 }
 
 local settings = {
@@ -59,3 +60,18 @@ for _, server in pairs(servers) do
 	end
 	lspconfig[server].setup(opts)
 end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client == nil then
+			return
+		end
+		if client.name == "ruff" then
+			-- Disable hover in favor of Pyright
+			client.server_capabilities.hoverProvider = false
+		end
+	end,
+	desc = "LSP: Disable hover capability from Ruff",
+})
